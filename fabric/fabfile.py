072d6a05ddfc3de1bf002457d1919dd8545bbf9a
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2014-07-08 00:03:18 +0100

    Creating variables and make the code more readable

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -6,46 +6,53 @@ from __future__ import with_statement
 from fabric.api import local, settings, abort, run, cd, put, lcd, sudo
 from fabric.contrib.console import confirm
 
-workPath = "./tmp/deployment"
-optPath = "/opt/freeview-api"
+localFolder="~/tmp"
+gitRepo="freview-api"
+gitUser="avilaplana"
+distName="surferstv-1.0-SNAPSHOT"
+
+remoteFolder="~/tmp"
+deployFolder="/opt/freview-api"
+serviceName="freeview-api"
+
 
 def clone():
-	local("rm -fr ./tmp")
-	local("mkdir -p " + workPath)
-	with lcd(workPath):
-		local("git clone git@github.com:avilaplana/freview-api.git")    
+	local("rm -fr " + localFolder)
+	local("mkdir -p " + localFolder)
+	with lcd(localFolder):
+		local("git clone git@github.com:" + gitUser + "/"+ gitRepo + '.git')    
 
-def tag():
-	with lcd(workPath + '/freview-api'):
-		local()
+# def tag():
+# 	with lcd(workPath + '/freview-api'):
+# 		local()
 
-def set_version():
-	with lcd(workPath + '/freview-api'):
-		local()
+# def set_version():
+# 	with lcd(workPath + '/freview-api'):
+# 		local()
 
 def dist():			
-	with lcd(workPath + '/freview-api'):
+	with lcd(localFolder + '/' + gitRepo):
 		local("sbt dist")
 
 def stop():
-	sudo('service freeview-api stop', pty=False)
+	sudo('service ' + serviceName + ' stop', pty=False)
 
 def start():
-	sudo('service freeview-api start', pty=False)
+	sudo('service ' + serviceName + ' start', pty=False)
 	
 def copy():
-	run('rm -fr ./tmp')
-	run('mkdir -p ' + workPath)
-	put(workPath + '/freview-api/target/universal/surferstv-1.0-SNAPSHOT.zip', './tmp')	
-	run('unzip ./tmp/surferstv-1.0-SNAPSHOT.zip -d ' + workPath)		
-	run('mv ' + workPath + '/surferstv-1.0-SNAPSHOT ' + workPath + '/freeview-api')
+	run('rm -fr ' + remoteFolder)
+	run('mkdir -p ' + remoteFolder)
+	put(localFolder + '/' + gitRepo + '/target/universal/' + distName + '.zip', remoteFolder) 
+	run('unzip '+ remoteFolder + '/' + distName + '.zip' +  ' -d ' + remoteFolder)		
+	run('mv ' + remoteFolder + '/' + distName + ' ' + remoteFolder + '/freeview-api')
 
 def deploy():
-	sudo('rm -fr ' + optPath)	
-	sudo('mv ' + workPath + '/freeview-api ' + optPath)
-	sudo('chown -R playframework:nogroup ' + optPath)
+	sudo('rm -fr ' + deployFolder)	
+	sudo('mv ' + remoteFolder + '/freeview-api ' + deployFolder)
+	sudo('chown -R playframework:nogroup ' + deployFolder)
 
-def all():
+def deploy_cloud():
 	clone()
 	dist()
 	copy()
