36016b9c83c2e9a3afae3cbfdf9f689981ed7b05
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2014-09-11 23:51:40 +0100

    Adding logs

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -7,12 +7,14 @@ import json
 
 
 def clone():
+	print "------ CLONE --------"
 	local("rm -fr " + localFolder)
 	local("mkdir -p " + localFolder)
 	with lcd(localFolder):
 		local("git clone https://github.com/" + gitUser + "/"+ gitRepo + '.git')    
 
 def tagging(tag = None):
+	print "------ TAGGING --------"
 	if tag:
 		print("Tagging to release/" + tag)
 		with lcd(localFolder + '/' + gitRepo):
@@ -22,7 +24,8 @@ def tagging(tag = None):
 		print("No Tagging")
 	
 
-def dist(tag = None):			
+def dist(tag = None):		
+	print "------ DIST --------"	
 	with lcd(localFolder + '/' + gitRepo):
 		if tag:
 			with shell_env(FREEAPI_VIEW_VERSION=tag):
@@ -32,12 +35,15 @@ def dist(tag = None):
 			local("sbt dist")
 					
 def stop():
+	print "------ STOP --------"
 	sudo('service ' + serviceName + ' stop', pty=False)
 
 def start():
+	print "------ START --------"
 	sudo('service ' + serviceName + ' start', pty=False)
 	
 def copy(tag = None):
+	print "------ COPY --------"
 	if tag:
 		version_to_use = tag
 	else:
@@ -50,6 +56,7 @@ def copy(tag = None):
 	run('mv ' + remoteFolder + '/' + distName + version_to_use +  ' ' + remoteFolder + '/freeview-api')
 
 def deploy():
+	print "------ DEPLOY --------"
 	sudo('rm -fr ' + deployFolder)	
 	sudo('mv ' + remoteFolder + '/freeview-api ' + deployFolder)
 	sudo('chown -R playframework:nogroup ' + deployFolder)
